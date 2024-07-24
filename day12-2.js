90e077e271f89d5836dba43a78caf2670a0b9d83
= <=>
2024-07-24 12:58:47 -0500

    day12 solutions - jeebus

--- day12-2.js
+++ day12-2.js
@@ -6,21 +6,19 @@ const lines = data.split('\n')
 lines.pop()
 
 const memo = {}
-
 const get_total_possibilites = (input, groups) => {
-
   if (input.startsWith('.')) {
     return get_total_possibilites(input.slice(1), groups)
   }
 
   // return stored value if we have it so that we do not need to do the work again
-  const key = `${input}-${groups.join(',')}`
+  const key = `${input} ${groups.join(',')}`
   if (memo[key] !== undefined) {
     return memo[key]
   }
 
   // if we have found all groups
-  if (groups.length === 0) {
+  if (groups.length <= 0) {
     // then if we still have springs then return 0 - invalid
     if (input.includes('#')) {
       return 0
@@ -58,11 +56,11 @@ const get_total_possibilites = (input, groups) => {
 
   // if the current character is not #, then check next substring
   // if a spring is at the current location then it would be part of the next match
-  if (input[0] != '#') {
+  if (input[0] !== '#') {
     total += get_total_possibilites(input.slice(1), groups)
   }
 
-  // otherwise if the group is valid and the next character is not #
+  // if the group is valid and the next character is not #
   // if the next character is # then it would be part of this match
   const next_character = input[current_group]
   if (valid && next_character !== '#') {
@@ -78,9 +76,11 @@ let total = 0
 
 for (const line of lines) {
   let [input, groups] = line.split(' ')
+  input = [input, input, input, input, input].join('?')
   input = input.replaceAll(/\.+/g, '.')
   groups = groups.split(',').map(x => parseInt(x))
-  const line_total = get_total_possibilites(input, groups)
+  groups = [groups, groups, groups, groups, groups].flat()
+  const line_total = get_total_possibilites(input, groups, {})
   total += line_total
 }
 
