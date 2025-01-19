ddaf671f99a4f4db3177647d2a5a0f95783f3324
= <=>
2025-01-19 00:33:04 -0600

    feat(2016): day 6 solutions

---
+++ 2016/day06-1.js
@@ -0,0 +1,23 @@
+import fs from 'node:fs'
+
+const data = fs.readFileSync('./data/input6.txt', 'utf-8')
+
+const lines = data.split('\n')
+lines.pop()
+const grid = lines.map(x => x.split(''))
+
+for (let i = 0; i < grid[0].length; i++) {
+  let map = {}
+  for (let j = 0; j < grid.length; j++) {
+    let key = grid[j][i]
+    if (map[key]) {
+      map[key]++
+    }
+    else {
+      map[key] = 1
+    }
+  }
+  const entries = Object.entries(map)
+  entries.sort((a, b) => b[1] - a[1])
+  console.log(entries[0])
+}
