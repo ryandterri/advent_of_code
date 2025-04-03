2bd8d7ffe382ab416c86fef9d462fb545c9278de
= <=>
2025-04-03 16:10:56 -0500

    feat(2017): day 1 solutions

---
+++ 2017/day01-1.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+
+const data = fs.readFileSync('./data/input.txt', 'utf-8')
+
+const arr = data.split('')
+arr.pop()
+
+const numbers_to_sum = []
+const max = arr.length
+for (let i = 0; i < arr.length; i++) {
+  const next_index = (i + 1) % max
+  const a = parseInt(arr[i])
+  const b = parseInt(arr[next_index])
+  if (a === b) {
+    numbers_to_sum.push(a)
+  }
+}
+
+console.log(numbers_to_sum.reduce((a, b) => a += b))
+
+process.exit()
